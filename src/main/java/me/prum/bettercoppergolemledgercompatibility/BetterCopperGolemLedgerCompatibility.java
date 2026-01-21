fb83d42dee9b8f9e9ae4bf52a983263a141f8c44
IxPrumxI <walidaqeel@gmail.com>
2026-01-21 21:22:19 +0300

    1.0.0

---
+++ src/main/java/me/prum/bettercoppergolemledgercompatibility/BetterCopperGolemLedgerCompatibility.java
@@ -0,0 +1,15 @@
+package me.prum.bettercoppergolemledgercompatibility;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterCopperGolemLedgerCompatibility implements DedicatedServerModInitializer {
+	public static final String MOD_ID = "bettercoppergolem-ledger-compatibility";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeServer() {}
+
+}
